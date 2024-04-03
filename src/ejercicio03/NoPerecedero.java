428a42c8b219eb64c66502b6537680f14d678c8c
nestorss <nestorss@nestorss-TUF>
2024-04-03 10:08:48 +0200

    Ejercicio 3 FINAL sin interfaz main

---
+++ src/ejercicio03/NoPerecedero.java
@@ -0,0 +1,88 @@
+package ejercicio03;
+
+/**
+ * Clase Perecedero
+ * 
+ * @author Nestor Sanchez
+ */
+public class NoPerecedero extends Productos {
+
+	/**
+	 * String tipo: tipo del producto
+	 */
+	private String tipo;
+
+	/**
+	 * Constructor con parametros
+	 * 
+	 * @param nombre nombre del producto
+	 * @param precio precio del producto
+	 * @param tipo   tipo del producto
+	 */
+	public NoPerecedero(String nombre, double precio, String tipo) {
+
+		// Asigna el nombre y el precio del producto
+		super(nombre, precio);
+
+		// Se guardara el tipo si no es nulo o no es cadena vacia
+		if (tipo != null && !tipo.equals("")) {
+			this.tipo = tipo;
+		}
+
+	}
+
+	/**
+	 * Getter getTipo
+	 * 
+	 * @return dias que le faltan al producto para caducar
+	 */
+	public String getDiasACaducar() {
+		return tipo;
+	}
+
+	/**
+	 * Setter setTipo
+	 * 
+	 * @param diasACaducar dias que le faltan al producto para caducar
+	 */
+	public void setDiasACaducar(String tipo) {
+		this.tipo = tipo;
+	}
+
+	/**
+	 * toString: incluye la informacion del producto a mostar por pantalla
+	 */
+	@Override
+	public String toString() {
+
+		// String texto: texto a pasar al main
+		String texto;
+
+		// texto sera el toString de la clase productos incluyendo su tipo
+		texto = super.toString() + " | Tipo del producto: " + tipo;
+
+		// Devuelve texto
+		return texto;
+
+	}
+
+	/**
+	 * double calcular: calcula el precio final del producto
+	 * 
+	 * @param cantidad cantidad del producto
+	 * @return precio final del producto
+	 */
+	public double calcular(int cantidad) {
+
+		// double precioFinal: precio final del producto
+		double precioFinal;
+
+		// El precio final sera lo que se calcule en la clase Productos
+		precioFinal = super.calcular(cantidad);
+
+		// Devuelve precioFinal al main
+		return precioFinal;
+
+	}
+
+}
---
+++ src/ejercicio03/Productos.java
@@ -0,0 +1,112 @@
+package ejercicio03;
+
+/**
+ * Clase Productos
+ * 
+ * @author Nestor Sanchez
+ */
+public class Productos {
+
+	/**
+	 * String nombre: nombre del producto
+	 */
+	protected String nombre;
+
+	/**
+	 * double precio: precio del producto
+	 */
+	protected double precio;
+
+	/**
+	 * Constructor con parametros
+	 * 
+	 * @param nombre nombre del producto
+	 * @param precio precio del producto
+	 */
+	public Productos(String nombre, double precio) {
+
+		// Se guardara el nombre si no es nulo o no es cadena vacia
+		if (nombre != null && !nombre.equals("")) {
+			this.nombre = nombre;
+		}
+
+		// Se guardara el precio si es mayor o igual a 0
+		if (precio >= 0) {
+			this.precio = precio;
+		}
+
+	}
+
+	/**
+	 * Getter getNombre
+	 * 
+	 * @return nombre del producto
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * Setter setNombre
+	 * 
+	 * @param nombre nombre del producto
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * Getter getPrecio
+	 * 
+	 * @return precio del producto
+	 */
+	public double getPrecio() {
+		return precio;
+	}
+
+	/**
+	 * Setter setPrecio
+	 * 
+	 * @param precio precio del producto
+	 */
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	/**
+	 * toString: incluye la informacion del producto a mostar por pantalla
+	 */
+	@Override
+	public String toString() {
+
+		// String texto: texto a pasar al main
+		String texto;
+
+		// El texto incluira el nombre y el precio
+		texto = "Nombre: " + nombre + " | Precio: " + precio + "€";
+
+		// Devuelve texto
+		return texto;
+
+	}
+
+	/**
+	 * double calcular: calcula el precio final del producto
+	 * 
+	 * @param cantidad cantidad del producto
+	 * @return precio final del producto
+	 */
+	public double calcular(int cantidad) {
+
+		// double precioFinal: precio final del producto
+		double precioFinal;
+
+		// precioFinal sera el precio por la cantidad
+		precioFinal = precio * cantidad;
+
+		// Devuelve precioFinal al main
+		return precioFinal;
+
+	}
+
+}
